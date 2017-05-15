cf6974345608aab1324dfb28f94a16bca4a4570e
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-05-15 14:21:22 +0200

    Add model definitions

--- election2000/models.py
+++ election2000/models.py
@@ -1,1 +1,44 @@
 from django.db import models
+
+class Candidate(models.Model):
+    first_name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+
+    class Meta:
+        unique_together = ('first_name', 'last_name')
+
+class Voivodeship(models.Model):
+    name = models.CharField(max_length = 256, unique = True)
+
+class District(models.Model):
+    number = models.IntegerField(unique = True)
+    voivodeship = models.ForeignKey("Voivodeship")
+
+class Gmina(models.Model):
+    code = models.IntegerField(unique = True)
+    name = models.CharField(max_length = 256)
+
+class Circuit(models.Model):
+    number = models.IntegerField()
+    district = models.ForeignKey("District")
+    gmina = models.ForeignKey("Gmina")
+
+    eligible = models.IntegerField(default = 0)
+    ballots_given_out = models.IntegerField(default = 0)
+    ballots_cast = models.IntegerField(default = 0)
+    ballots_invalid = models.IntegerField(default = 0)
+    ballots_valid = models.IntegerField(default = 0)
+
+    document = models.FileField(upload_to = 'documents', default = None,
+            null = True)
+
+    class Meta:
+        unique_together = ('number', 'district', 'gmina')
+
+class Votes(models.Model):
+    candidate = models.ForeignKey("Candidate")
+    circuit = models.ForeignKey("Circuit")
+    number = models.IntegerField()
+
+    class Meta:
+        unique_together = ('candidate', 'circuit')
