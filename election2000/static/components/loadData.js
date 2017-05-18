9a439ba02e8dafb1737471e38a1d0c782a8db11f
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-05-18 21:43:17 +0200

    Save downloaded data to localStorage

--- election2000/static/components/loadData.js
+++ election2000/static/components/loadData.js
@@ -1,8 +1,15 @@
 export const loadData = (path, onReceiveData) => {
+  let data = localStorage.getItem(path)
+  if (data) {
+    onReceiveData(JSON.parse(data))
+  }
+
   let xhr = new XMLHttpRequest()
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      onReceiveData(JSON.parse(xhr.responseText))
+      let data = xhr.responseText
+      localStorage.setItem(path, data)
+      onReceiveData(JSON.parse(data))
     }
   }
 
